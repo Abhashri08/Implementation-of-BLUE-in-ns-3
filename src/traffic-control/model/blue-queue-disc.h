57a6e9407463c05146aa6daa1558286e09f77db8
vivek <jain.vivek.anand@gmail.com>
2016-09-30 12:55:48 +0530

    Updated Header file for BLUE

--- src/traffic-control/model/blue-queue-disc.h
+++ src/traffic-control/model/blue-queue-disc.h
@@ -26,8 +26,8 @@
  * Most of the comments are also ported from the same.
  */
 
-#ifndef PIE_QUEUE_DISC_H
-#define PIE_QUEUE_DISC_H
+#ifndef BLUE_QUEUE_DISC_H
+#define BLUE_QUEUE_DISC_H
 
 #include <queue>
 #include "ns3/packet.h"
@@ -46,158 +46,33 @@ namespace ns3 {
 class TraceContainer;
 class UniformRandomVariable;
 
-/**
- * \ingroup traffic-control
- *
- * \brief Implements PIE Active Queue Management discipline
- */
-class PieQueueDisc : public QueueDisc
+class BlueQueueDisc : public QueueDisc
 {
 public:
-  /**
-   * \brief Get the type ID.
-   * \return the object TypeId
-   */
-  static TypeId GetTypeId (void);
-
-  /**
-   * \brief PieQueueDisc Constructor
-   */
-  PieQueueDisc ();
-
-  /**
-   * \brief PieQueueDisc Destructor
-   */
-  virtual ~PieQueueDisc ();
-
-  /**
-   * \brief Stats
-   */
-  typedef struct
-  {
-    uint32_t unforcedDrop;      //!< Early probability drops: proactive
-    uint32_t forcedDrop;        //!< Drops due to queue limit: reactive
-  } Stats;
-
-  /**
-   * \brief Burst types
-   */
-  enum BurstStateT
-  {
-    NO_BURST,
-    IN_BURST,
-    IN_BURST_PROTECTING,
-  };
-
-  /**
-   * \brief Set the operating mode of this queue.
-   *
-   * \param mode The operating mode of this queue.
-   */
-  void SetMode (Queue::QueueMode mode);
-
-  /**
-   * \brief Get the encapsulation mode of this queue.
-   *
-   * \returns The encapsulation mode of this queue.
-   */
-  Queue::QueueMode GetMode (void);
-
-  /**
-   * \brief Get the current value of the queue in bytes or packets.
-   *
-   * \returns The queue size in bytes or packets.
-   */
-  uint32_t GetQueueSize (void);
-
-  /**
-   * \brief Set the limit of the queue in bytes or packets.
-   *
-   * \param lim The limit in bytes or packets.
-   */
-  void SetQueueLimit (uint32_t lim);
-
-  /**
-   * \brief Get queue delay
-   */
-  Time GetQueueDelay (void);
-
-  /**
-   * \brief Get PIE statistics after running.
-   *
-   * \returns The drop statistics.
-   */
-  Stats GetStats ();
-
-  /**
-   * Assign a fixed random variable stream number to the random variables
-   * used by this model.  Return the number of streams (possibly zero) that
-   * have been assigned.
-   *
-   * \param stream first stream index to use
-   * \return the number of stream indices assigned by this model
-   */
-  int64_t AssignStreams (int64_t stream);
-
-protected:
-  /**
-   * \brief Dispose of the object
-   */
-  virtual void DoDispose (void);
+  BlueQueueDisc ();
+  ~BlueQueueDisc ();
 
 private:
-  virtual bool DoEnqueue (Ptr<QueueDiscItem> item);
-  virtual Ptr<QueueDiscItem> DoDequeue (void);
-  virtual Ptr<const QueueDiscItem> DoPeek (void) const;
-  virtual bool CheckConfig (void);
+	int bytes;                              //??
+	int dummy;                              //??
+	int setbit;                             // Whether to Use ECN (Cannot use this because ns-3 doesn't have support for ECN)
+	int mean_pktsize;                       // Average Packet Size
+	double decrement;                       // marking probability decrement value
+	double increment;                       // marking probability increment value
+	double iholdtime;                       // last time at which pmark incremented 
+	double dholdtime;                       // last time at which pmark decremented
+	int dalgorithm;                         //??
+	int ialgorithm;                         //??
+	double bandwidth;                       //??
 
-  /**
-   * \brief Initialize the queue parameters.
-   */
-  virtual void InitializeParams (void);
+	int idle;                               //??
+	double idletime;                        //??
+	double ptc;                             //??
+	double ifreezetime_;                    // Time interval during which pmark_ cannot be increased
+	double dfreezetime_;                    // Time interval during which pmark_ cannot be decreased
+	double pmark_;                          // Marking Probability
 
-  /**
-   * \brief Check if a packet needs to be dropped due to probability drop
-   * \param item queue item
-   * \param qSize queue size
-   * \returns 0 for no drop, 1 for drop
-   */
-  bool DropEarly (Ptr<QueueDiscItem> item, uint32_t qSize);
 
-  /**
-   * Periodically update the drop probability based on the delay samples:
-   * not only the current delay sample but also the trend where the delay
-   * is going, up or down
-   */
-  void CalculateP ();
-
-  Stats m_stats;                                //!< PIE statistics
-
-  // ** Variables supplied by user
-  Queue::QueueMode m_mode;                      //!< Mode (bytes or packets)
-  uint32_t m_queueLimit;                        //!< Queue limit in bytes / packets
-  Time m_sUpdate;                               //!< Start time of the update timer
-  Time m_tUpdate;                               //!< Time period after which CalculateP () is called
-  Time m_qDelayRef;                             //!< Desired queue delay
-  uint32_t m_meanPktSize;                       //!< Average packet size in bytes
-  Time m_maxBurst;                              //!< Maximum burst allowed before random early dropping kicks in
-  double m_a;                                   //!< Parameter to pie controller
-  double m_b;                                   //!< Parameter to pie controller
-  uint32_t m_dqThreshold;                       //!< Minimum queue size in bytes before dequeue rate is measured
-
-  // ** Variables maintained by PIE
-  double m_dropProb;                            //!< Variable used in calculation of drop probability
-  Time m_qDelayOld;                             //!< Old value of queue delay
-  Time m_qDelay;                                //!< Current value of queue delay
-  Time m_burstAllowance;                        //!< Current max burst value in seconds that is allowed before random drops kick in
-  uint32_t m_burstReset;                        //!< Used to reset value of burst allowance
-  BurstStateT m_burstState;                     //!< Used to determine the current state of burst
-  bool m_inMeasurement;                         //!< Indicates whether we are in a measurement cycle
-  double m_avgDqRate;                           //!< Time averaged dequeue rate
-  double m_dqStart;                             //!< Start timestamp of current measurement cycle
-  uint32_t m_dqCount;                           //!< Number of bytes departed since current measurement cycle starts
-  EventId m_rtrsEvent;                          //!< Event used to decide the decision of interval of drop probability calculation
-  Ptr<UniformRandomVariable> m_uv;              //!< Rng stream
 };
 
 };   // namespace ns3
